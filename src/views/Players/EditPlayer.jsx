1e50ebcc57ab27cab6bd0f5d2c670df3331ea479
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-12-17 12:15:42 -0800

    updated editPlayer JSX

--- src/views/Players/EditPlayer.jsx
+++ src/views/Players/EditPlayer.jsx
@@ -1,36 +1,50 @@
-import { React, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import PlayerForm from '../../components/Players/PlayerForm';
-import { updatePlayerById } from '../../services/players';
+import { getPlayerById, updatePlayerById } from '../../services/players';
 
-function EditPlayer({ match }) {
-  const { playerId } = match.params;
+function EditPlayer() {
+  const { id } = useParams();
   const [name, setName] = useState('');
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
   const [team, setTeam] = useState('');
+  const [position, setPosition] = useState('');
+  const [loading, setLoading] = useState(true);
   const history = useHistory();
+
+  useEffect(() => {
+    async function getPlayerData() {
+      const { name, position } = await getPlayerById(id);
+      setName(name);
+      setPosition(position);
+      setLoading(false);
+    }
+    getPlayerData();
+  }, [id]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const resp = await updatePlayerById(playerId, { name, city, state, team });
+    const resp = await updatePlayerById(id, { name, city, state, team });
     history.push(`/players/${resp[0].id}`);
   };
 
   return (
     <>
       <fieldset>
-        <legend>Edit a team</legend>
+        <legend>Edit a player</legend>
         <PlayerForm
           name={name}
           city={city}
           state={state}
           team={team}
+          position={position}
           submitHandler={onSubmit}
           setName={setName}
           setCity={setCity}
           setState={setState}
           setTeam={setTeam}
+          setPosition={setPosition}
         />
       </fieldset>
       <Link to="/" className="App-link">
